75bcac0b124ad4000ad5682cdb7fcbcaa37a033d
elody-ramirez <elody.ramirez@gmail.com>
2022-08-08 01:24:41 -0400

    initial commit for sudoku app with initial board

--- src/App.js
+++ src/App.js
@@ -1,25 +1,141 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class Square extends React.Component {
+	render() {
+		return <button className='square'>{/* TODO */}</button>
+	}
 }
 
-export default App;
+class Board extends React.Component {
+	renderSquare(i) {
+		return <Square />
+	}
+
+	render() {
+		const status = 'Sudoku'
+
+		return (
+			<div>
+				<div className='status'>{status}</div>
+				<div className='board-row'>
+					{this.renderSquare(0)}
+					{this.renderSquare(1)}
+					{this.renderSquare(2)}
+					{this.renderSquare(3)}
+					{this.renderSquare(4)}
+					{this.renderSquare(5)}
+					{this.renderSquare(6)}
+					{this.renderSquare(7)}
+					{this.renderSquare(8)}
+				</div>
+				<div className='board-row'></div>
+                    {this.renderSquare(9)}
+					{this.renderSquare(10)}
+					{this.renderSquare(11)}
+					{this.renderSquare(12)}
+					{this.renderSquare(13)}
+					{this.renderSquare(14)}
+					{this.renderSquare(15)}
+					{this.renderSquare(16)}
+					{this.renderSquare(17)}
+				<div className='board-row'></div>
+                <div className='board-row'></div>
+                    {this.renderSquare(18)}
+					{this.renderSquare(19)}
+					{this.renderSquare(20)}
+					{this.renderSquare(21)}
+					{this.renderSquare(22)}
+					{this.renderSquare(23)}
+					{this.renderSquare(24)}
+					{this.renderSquare(25)}
+					{this.renderSquare(26)}
+				<div className='board-row'></div>
+                <div className='board-row'></div>
+                    {this.renderSquare(27)}
+					{this.renderSquare(28)}
+					{this.renderSquare(29)}
+					{this.renderSquare(30)}
+					{this.renderSquare(31)}
+					{this.renderSquare(32)}
+					{this.renderSquare(33)}
+					{this.renderSquare(34)}
+					{this.renderSquare(35)}
+				<div className='board-row'></div>
+                <div className='board-row'></div>
+                    {this.renderSquare(36)}
+					{this.renderSquare(37)}
+					{this.renderSquare(38)}
+					{this.renderSquare(39)}
+					{this.renderSquare(40)}
+					{this.renderSquare(41)}
+					{this.renderSquare(42)}
+					{this.renderSquare(43)}
+					{this.renderSquare(44)}
+				<div className='board-row'></div>
+                <div className='board-row'></div>
+                    {this.renderSquare(45)}
+					{this.renderSquare(46)}
+					{this.renderSquare(47)}
+					{this.renderSquare(48)}
+					{this.renderSquare(49)}
+					{this.renderSquare(50)}
+					{this.renderSquare(51)}
+					{this.renderSquare(52)}
+					{this.renderSquare(53)}
+				<div className='board-row'></div>
+                <div className='board-row'></div>
+                    {this.renderSquare(54)}
+					{this.renderSquare(55)}
+					{this.renderSquare(56)}
+					{this.renderSquare(57)}
+					{this.renderSquare(58)}
+					{this.renderSquare(59)}
+					{this.renderSquare(60)}
+					{this.renderSquare(61)}
+					{this.renderSquare(62)}
+				<div className='board-row'></div>
+                <div className='board-row'></div>
+                    {this.renderSquare(63)}
+					{this.renderSquare(64)}
+					{this.renderSquare(65)}
+					{this.renderSquare(66)}
+					{this.renderSquare(67)}
+					{this.renderSquare(68)}
+					{this.renderSquare(69)}
+					{this.renderSquare(70)}
+					{this.renderSquare(71)}
+				<div className='board-row'></div>
+                <div className='board-row'></div>
+                    {this.renderSquare(72)}
+					{this.renderSquare(73)}
+					{this.renderSquare(74)}
+					{this.renderSquare(75)}
+					{this.renderSquare(76)}
+					{this.renderSquare(77)}
+					{this.renderSquare(78)}
+					{this.renderSquare(79)}
+					{this.renderSquare(80)}
+				<div className='board-row'></div>
+			</div>
+		)
+	}
+}
+
+class Game extends React.Component {
+	render() {
+		return (
+			<div className='game'>
+				<div className='game-board'>
+					<Board />
+				</div>
+				<div className='game-info'>
+					<div>{/* status */}</div>
+					<ol>{/* TODO */}</ol>
+				</div>
+			</div>
+		)
+	}
+}
+
+export default Game

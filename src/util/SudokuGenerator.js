600076a13a17ced675aead9b21b5d241772c7e48
elody-ramirez <elody.ramirez@gmail.com>
2022-08-08 23:43:04 -0400

    cleaned up code and comments

--- src/util/SudokuGenerator.js
+++ src/util/SudokuGenerator.js
@@ -5,12 +5,12 @@ function randomChoice(choices) {
 	return choices[Math.floor(Math.random() * choices.length)]
 }
 
-// helper function to clean up makePuzzle()
+// helper function to clean up makePuzzle() that returns an array of length n
 function range(n) {
 	return Array.from(Array(n).keys())
 }
 
-// helper function to clean up makePuzzle()
+// helper function to clean up makePuzzle() that returns an array of 9 sets
 function set() {
     return Array.from(Array(9).keys()).map(
 			() => new Set([1, 2, 3, 4, 5, 6, 7, 8, 9])
@@ -24,17 +24,14 @@ function makePuzzle() {
     // Continues looping until all 81 cells are filled
 	while (true) {
 		try {
-			// Creates a 2D Array that is 9x9
+			// Creates a 2D 9x9 Array & set of available numbers for each row, column and square (3x3 section)
 			const puzzle = range(9).map(() => range(9))
-			// Set of available numbers for each row
 			const rows = set()
-			// Set of available numbers for each column
 			const columns = set()
-			// Set of available numbers for each square (3x3 section)
 			const squares = set()
 			Array.from(Array(9).keys()).forEach((i) => {
 				Array.from(Array(9).keys()).forEach((j) => {
-					// this checks the available numbers for cell(i, j) looking at the row, column and square relative to that cell 
+					// this checks the available numbers for cell(i, j) looking at the row, column and square relative to that cell
 					const row = rows[i]
 					const column = columns[j]
 					const square = squares[Math.floor(i / 3) * 3 + Math.floor(j / 3)]
@@ -46,9 +43,8 @@ function makePuzzle() {
 						// eslint-disable-next-line no-throw-literal
 						throw 'dead end'
 					}
-                    // adds random choice to puzzle
+					// adds random choice to puzzle & removes availability in the set of columns, rows & squares
 					puzzle[i][j] = choice
-                    // deletes this random choice from available numbers in the set of columns, rows & squares
 					column.delete(choice)
 					row.delete(choice)
 					square.delete(choice)

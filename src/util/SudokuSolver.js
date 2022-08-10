8a91671f1f1be42c8f5bec13780aeb75d2b612b0
elody-ramirez <elody.ramirez@gmail.com>
2022-08-09 23:32:06 -0400

    Added comments

--- src/util/SudokuSolver.js
+++ src/util/SudokuSolver.js
@@ -1,5 +1,5 @@
 
-
+// This is a helper function I will use later to take in the current board and compare it to 3 different arrays (rows, columns & squares) to make sure there are no duplicates.
 const hasDuplicates = (arr, board) => {
 	const storage = {}
 	for (let i = 0; i < arr.length; i++) {

16bf892829ab839c236aa6b41a5e36479c765df5
elody-ramirez <elody.ramirez@gmail.com>
2022-08-27 18:40:43 -0400

    Changed app to functional component as well

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,13 @@
-import React, {Component} from 'react'
-
 import Game from './Game'
 
-class App extends Component {
-	render () {
-		return (
-			<div>
-				<div className='game-board'>
-					<Game />
-				</div>
+const App = () => {
+	return (
+		<div>
+			<div className='game-board'>
+				<Game />
 			</div>
-		)
-
-	}
+		</div>
+	)
 }
 
 export default App

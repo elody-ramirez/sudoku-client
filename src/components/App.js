b7ec231454dfd39a10d89891ea62ddd5976ebd13
elody-ramirez <elody.ramirez@gmail.com>
2022-08-27 18:42:56 -0400

    removed extra div that isnt currently needed in App

--- src/components/App.js
+++ src/components/App.js
@@ -2,10 +2,8 @@ import Game from './Game'
 
 const App = () => {
 	return (
-		<div>
-			<div className='game-board'>
-				<Game />
-			</div>
+		<div className='game-board'>
+			<Game />
 		</div>
 	)
 }

565f480066583b6b53947705e94818dc3f0f69d5
elody-ramirez <elody.ramirez@gmail.com>
2022-08-28 21:24:01 -0400

    Cleaned up initial useEffect hook

--- src/components/Board.js
+++ src/components/Board.js
@@ -6,6 +6,17 @@ const Board = () => {
 	const [board, setBoard] = useState([])
 
 	useEffect(() => {
+		setUpBoard()
+	// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, [])	
+
+	function renderSquare(i, k) {
+		return (
+			<Square key={k} initialValue={i} changeable={i === '' ? true : false} />
+		)
+	}
+	
+	const setUpBoard = () => {
 		let b = []
 		let puzzle = Puzzles[1]
 		let row
@@ -13,22 +24,12 @@ const Board = () => {
 		for (let i = 0; i < 9; i++) {
 			row = []
 			for (let j = 0; j < 9; j++) {
-				row.push(renderSquare(puzzle[count][1],count))
+				row.push(renderSquare(puzzle[count][1], count))
 				count++
 			}
 			b.push(<div key={i}>{row}</div>)
 		}
 		setBoard(b)
-	}, [])	
-
-	function renderSquare(i,k) {
-		return (
-			<Square
-				key={k}
-				initialValue={i}
-				changeable={ i === '' ? true : false}
-			/>
-		)
 	}
 
 	return (

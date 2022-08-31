9e2ea1a1d06238e50ebbb713a9f7cc1d2e3014dc
elody-ramirez <elody.ramirez@gmail.com>
2022-08-31 00:28:19 -0400

    removed the setUpBoard function outside the component so that it is initialized in the useState parameters without having to use useEffect

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,37 +1,34 @@
-import { useState, useEffect } from 'react'
+import { useState } from 'react'
 import Square from './Square'
 import { Puzzles } from '../util/Puzzles.js'
 
-const Board = () => {
-	const [board, setBoard] = useState([])
-
-	useEffect(() => {
-		setUpBoard()
-	// eslint-disable-next-line react-hooks/exhaustive-deps
-	}, [])	
-
-	const updateBoard = (squareID, value) => {
-		// const newBoard = board.map((row) => row.map((square) => 
-		// 	square.id === squareID ? alert(square) : square
-		// ))
-		console.log('test')
-
+const setUpBoard = () => {
+	const puzzle = Puzzles[1]
+	const rows = 9
+	const cols = 9
+	const result = new Array(rows)
+	for (let row = 0; row < rows; row++) {
+		result[row] = new Array(cols)
 	}
-	
-	const setUpBoard = () => {
-		const puzzle = Puzzles[1]
-		const rows = 9
-		const cols = 9
-		const result = new Array(rows)
-		for (let row = 0; row < rows; row++) {
-			result[row] = new Array(cols)
+	for (let row = 0; row < rows; row++) {
+		for (let col = 0; col < cols; col++) {
+			result[row][col] = puzzle[row * cols + col][1]
 		}
-		for (let row = 0; row < rows; row++) {
-			for (let col = 0; col < cols; col++) {
-				result[row][col] = puzzle[row * cols + col][1]
-			}
-		}
-		setBoard(result)
+	}
+	return result
+}
+
+const Board = () => {
+	const [board, setBoard] = useState(setUpBoard)
+
+	const updateBoard = (target, value) => {
+		const tempBoard = board
+		const newBoard = board.map((row, rowIndex) =>
+			row.map((col, colIndex) => 
+				target.id === JSON.stringify(rowIndex * 9 + (colIndex + 1) - 1) ? tempBoard[rowIndex][colIndex] = value : board[rowIndex][colIndex]
+			)
+		)
+		setBoard(newBoard)
 	}
 
 	return (
@@ -43,9 +40,9 @@ const Board = () => {
 						{row.map((value, colIndex) => (
 							<Square
 								key={rowIndex * 9 + (colIndex + 1)}
-								squareID={'square' + (rowIndex * 9 + (colIndex + 1))}
-								initialValue={value}
-								changeable={value === '' ? true : false}
+								squareID={(rowIndex * 9 + (colIndex + 1)) - 1}
+								initialValue={value !== 0 ? value : null}
+								changeable={value !== 0 ? true : false}
 								updateBoard={updateBoard}
 							/>
 						))}
